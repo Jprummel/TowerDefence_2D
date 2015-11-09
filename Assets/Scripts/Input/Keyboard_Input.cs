d26d21a29bb8ccd7522fbcfc11b2655a94b433b4
RRH707 <roushan_hausil@hotmail.com>
2015-11-09 08:41:34 +0100

    Player movement and Proto Attacks

--- Assets/Scripts/Input/Keyboard_Input.cs
+++ Assets/Scripts/Input/Keyboard_Input.cs
@@ -1,25 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Keyboard_Input : MonoBehaviour {
+public class Keyboard_Input : MonoBehaviour
+{
+    private float walkSpeed;
+    private float curSpeed;
+    private float maxSpeed;
 
-    public bool wPressed;
-    public bool aPressed;
-    public bool sPressed;
-    public bool dPressed;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-    void CheckKeyPress()
+     void Start()
     {
-       
+        walkSpeed = 5.0f;
+    }
+
+    void FixedUpdate()
+    {
+        curSpeed = walkSpeed;
+        maxSpeed = curSpeed;
+
+        
+        this.GetComponent<Rigidbody2D>().velocity = 
+        new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
+        Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
     }
 }
