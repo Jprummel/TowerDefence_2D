c5466181a092f2176a3f6f5fab7f947898d76e0a
Jprummel <jordiprummel@live.nl>
2015-11-13 19:17:14 +0100

    Made a start on placement
    Made a start on tower placement
    edited tower prefabs

--- Assets/Scripts/Towers/PlaceTower.cs
+++ Assets/Scripts/Towers/PlaceTower.cs
@@ -3,6 +3,24 @@ using System.Collections;
 
 public class PlaceTower : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject _towerPrefab;
+    private GameObject _tower;
+    
+    private bool _canPlaceTower()
+    {
+        return _tower == null;
+    }
+
+    void OnMouseUp()
+    {
+        if(_canPlaceTower())
+        {
+            _tower = (GameObject)
+                Instantiate(_towerPrefab, transform.position, Quaternion.identity);
+            //Reduce player gold TODO
+        }
+    }
 	// Use this for initialization
 	void Start () {
 	
