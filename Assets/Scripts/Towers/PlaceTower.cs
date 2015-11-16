a17c28d24f239862408fce605ce8312699ed4dbf
Jprummel <jordiprummel@live.nl>
2015-11-16 19:50:30 +0100

    Changed the prefabs / Almost done with upgrade script
    -Almost finished upgrade script (only need to add stat changes and gold
    cost when gold script is made)

--- Assets/Scripts/Towers/PlaceTower.cs
+++ Assets/Scripts/Towers/PlaceTower.cs
@@ -6,10 +6,24 @@ public class PlaceTower : MonoBehaviour {
     [SerializeField]
     private GameObject _towerPrefab;
     private GameObject _tower;
-    
+
+    private bool _canUpgradeTower()
+    {
+        if (_tower != null) //Checks if there is a tower to upgrade if it exists gets the current level
+        {
+            TowerData towerData = _tower.GetComponent<TowerData>();
+            TowerLevel nextLevel = towerData.getNextLevel(); //upgrades to next level
+            if (nextLevel != null)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private bool _canPlaceTower()
     {
-        return _tower == null;
+        return _tower == null; //Checks if the spot is taken already
     }
 
     void OnMouseUp()
@@ -18,16 +32,12 @@ public class PlaceTower : MonoBehaviour {
         {
             _tower = (GameObject)
                 Instantiate(_towerPrefab, transform.position, Quaternion.identity);
-            //Reduce player gold TODO
+            //Reduce player gold (To do)
+        }
+        else if (_canUpgradeTower())
+        {
+            _tower.GetComponent<TowerData>().increaseLevel();
+           // reduce gold again for upgrade (To do)
         }
     }
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
